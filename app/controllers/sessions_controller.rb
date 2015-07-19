756bc0fd9f380429fabf862b19969f4901007d0c
Darya Murauyova <muravkina@yahoo.com>
2015-07-19 19:14:49 -0400

    log in errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
 
   def new
+    if current_user
+      redirect_to current_user
+    end
   end
 
   def create
@@ -8,19 +11,28 @@ class SessionsController < ApplicationController
       user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
       @invited_challenges = Challenge.where({challenger_email: request.env["omniauth.auth"].info.email})
-    else
-      user = User.find_by({email: params[:email]})
-      session[:user_id] = user.id
-      @invited_challenges = Challenge.where({challenger_email: params[:email]})
-    end
-    if !@invited_challenges.empty?
+      if !@invited_challenges.empty?
       @invited_challenges.each do | challenge |
         challenge.challenger_id = session[:user_id]
         challenge.save
       end
     end
     redirect_to user
+    elsif User.find_by({email: params[:email]})
+      user = User.find_by({email: params[:email]})
+      session[:user_id] = user.id
+      @invited_challenges = Challenge.where({challenger_email: params[:email]})
+      if !@invited_challenges.empty?
+      @invited_challenges.each do | challenge |
+        challenge.challenger_id = session[:user_id]
+        challenge.save
+      end
+    end
+    redirect_to user
+  else
+    redirect_to '/'
   end
+end
 
   def destroy
     session[:user_id] = nil
